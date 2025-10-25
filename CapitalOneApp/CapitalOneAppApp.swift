f146dde88a5c835df63a5c050ca7093a3a285f0d
Bryan Meza <bryanmeza1414@gmail.com>
2025-10-25 00:48:38 -0600

    Initial Commit

---
+++ CapitalOneApp/CapitalOneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalOneAppApp.swift
+//  CapitalOneApp
+//
+//  Created by Bryan Meza on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CapitalOneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
