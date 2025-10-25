4dcde61930b4c6c58a6bfef7534ee53996036afa
zamer22 <angelitos22munoz@gmail.com>
2025-10-25 15:27:16 -0600

    integración log in

---
+++ Modelo CapitalOne copy/Modelo CapitalOne/Modelo_CapitalOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modelo_CapitalOneApp.swift
+//  Modelo CapitalOne
+//
+//  Created by Angel Aarón Muñoz Alvarez on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Modelo_CapitalOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
